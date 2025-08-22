5c994e6a9c7e8b70b03c70420a61d4ca79be3dd2
palak-kaushal <kaushalpalak2005@gmail.com>
2025-08-22 15:05:06 +0530

    add files

--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -3,106 +3,250 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
-const createUser = async(req,res) =>{
-    try{
-        const {name,email,address,contact,password} = req.body
-        const validation = []
-        if(!name || typeof name !== "string"){
-            validation.push("name is required and type must be string")
-        }
-        if(!email || typeof email !== "string"){
-            validation.push("email is required and type must be string")
-        }
-        if(!address || typeof address !== "string"){
-            validation.push("address is required and type must be string")
-        }
-        if(!contact || typeof contact !== "string"){
-            validation.push("conatct is required and type must be string")
-        }
-        if(!password || typeof password !== "string"){
-            validation.push("password is required and type must be string")
-        }
 
-        if(validation.length>0){
+const createUser = async(req, res) => {
+    try {
+        const { name, email, address, contact, password } = req.body
+        const validation = []
+
+        if (!name || typeof name !== "string") {
+            validation.push("name is required and type must be string ")
+        }
+        if (!email || typeof email !== "string") {
+            validation.push("email is required and type must be string ")
+        }
+        if (!address || typeof address !== "string") {
+            validation.push("address is required and type must be string ")
+        }
+        if (!contact || typeof contact !== "string") {
+            validation.push("contact is required and type must be string ")
+        }
+        if (!password || typeof password !== "string") {
+            validation.push("password is required and type must be string ")
+        }
+        if (validation.length > 0) {
             return res.json({
-                status:400,
-                success:false,
-                message:"validation error",
-                error:validation
+                status: 400,
+                success: false,
+                message: "validation error",
+                error: validation
             })
         }
 
         const hashedPassword = await bcrypt.hash(password,10)
 
+
         const user = new User({
-            name:name,
-            email:email,
-            password:hashedPassword,
-            address:address,
-            contact:contact
+            name: name,
+            email: email,
+            password: hashedPassword,
+            address: address,
+            contact: contact
         })
 
         await user.save()
         res.json({
-            status:201,
+            status: 201,
+            success: true,
+            message: "new user is create successfully",
+            data: user
+        })
+    } catch (err) {
+        res.json({
+            status: 500,
+            success: false,
+            message: "internal server error",
+            error: err.message
+        })
+
+    }
+}
+
+const loginUser = async (req,res) =>{
+    try{
+        const {name,email,password} = req.body
+        const user = await User.findOne({name, email})
+        if (!user){
+            return res.json({
+                status:404,
+                success:g=false,
+                message:"it is not a valid user"
+            })
+        }
+
+        const matchPassword = await bcrypt.compare(password,user.password)
+        if(!matchPassword){
+            return res.json({
+                status:404,
+                success:false,
+                message:"password is not valid"
+
+            })
+        }
+
+        const token = jwt.sign(
+            {userId:user._id,userName:user.name, userEmail:user.email},
+            process.env.SECRET_KEY,
+            {expiresIn:"1d"}
+        )
+        res.json({
+            status:200,
             success:true,
-            message:"new user is create successfully",
+            message:"user is login succesfully",
+            token
+        })
+    }
+    catch(err){
+        res.json({
+            status:500,
+            success:false,
+            message:"user is not login",
+            error:err.message
+        })
+
+    }
+}
+
+const getAllUser = async(req,res) =>{
+    try{
+        const user = await User.find()
+        res.json({
+            status:200,
+            success:true,
+            message:"all user get succesfully",
             data:user
         })
 
-    }catch(err){
-          res.json({
+    }
+    catch(err){
+        res.json({
+            status:500,
+            success:false,
+            message:"internal server error",err,
+            error:err.message
+        })
+
+    }
+}
+
+//get user by id
+
+const getUserById = async(req,res) =>{
+    try{
+        const{id} = req.body
+            if(!id){
+                return res.json({
+                    status:400,
+                    success:false,
+                    message:"id is required"
+                })
+            }
+
+        
+        const user = await User.findById(id)
+        if(!user){
+             return res.json({
+                    status:400,
+                    success:false,
+                    message:"id is wrong"
+                })
+
+        }
+        res.json({
+            status:200,
+            success:true,
+            message:"user is get successfully",
+            data : user
+        })
+
+    }
+    catch(err){
+        res.json({
             status:500,
             success:false,
             message:"internal server error",
             error:err.message
-          })
+        })
+
+    }
+}
+//update user by id
+const updateUserById = async (req,res) => {
+    try{
+        const {id,...data} = req.body
+        if(!id){
+            return res.json({
+                status:400,
+                success:false,
+                message:"id is required"
+            })
+        }
+        const user = await User.findByIdAndUpdate(id,data,{new:true})
+        if(!user){
+            res.json({
+                status:400,
+                success:false,
+                message:"id is wrong"
+            })
+        }
+        res.json({
+            status:200,
+            success:false,
+            message:"user is updated successfully",
+            data:user
+        })
+
+    }
+
+    catch(err){
+        res.json({
+            status:500,
+            success:false,
+            message:"internal server error",
+            error:err.message
+        })
+
     }
 }
 
-const loginUser = async(req,res) =>{
+//delete user by id
+
+const deleteUserById = async(req,res) =>{
     try{
-        const {email,password} = req.body 
-        const user = await User.findOne({email})
-        if(!user){
+        const {id} = req.body
+        if(!id){
             return res.json({
-                status:404,
+                status:400,
                 success:false,
-                message:"it is not valid user"
+                message:"id is required"
             })
         }
 
-       // matched password 
-       const matchPassword = await bcrypt.compare(password,user.password)
-       if(!matchPassword){
-        return res.json({
-            status:404,
-            success:false,
-            message:"password is not valid"
-        })
-       }
+        const deleteUser = await User.findByIdAndDelete(id)
+        if(!deleteUser){
+            res.json({
+                status:400,
+                success:false,
+                message:"id is wrong or document is not exist"
+            })
+        }
 
-       //create a token 
-const token = jwt.sign(
-    {userId:user._id,userEmail:user.email},
-    process.env.SECRET_KEY,
-    {expiresIn:"1d"}
-)
-res.json({
-    status:200,
-    success:true,
-    message:"user is login successfully",
-    token
-})
-    }catch(err){
-res.json({
-    status:500,
-    success:false,
-    message:"internal server error",
-    error:err.message
-})
+        res.json({
+            status:200,
+            success:true,
+            message:"user is deleted successfully",
+            data:deleteUser
+        })
+
+    }
+    catch(err){
+        res.json({
+            status:500,
+            success:false,
+            message:"internal server error",
+            error:err.message
+        })
+
     }
 }
-
-
-module.exports = {createUser,loginUser}
+module.exports = { createUser , loginUser, getAllUser ,getUserById, updateUserById,deleteUserById}
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const {createUser,loginUser} = require("../api/user/userController")
+const {createUser,loginUser ,getAllUser,getUserById ,updateUserById,deleteUserById} = require("../api/user/userController")
 
 router.post("/createuser",createUser)
 router.post("/loginuser",loginUser)
+router.post("/getAlluser",getAllUser)
+router.post("/getuserByid",getUserById)
+router.post("/updateUserById",updateUserById)
+router.post("/deleteUserById",deleteUserById)
+
 
 
 

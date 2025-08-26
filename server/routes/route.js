b2b6c8633e407927783ec9189ccad27d0b85c855
palak-kaushal <kaushalpalak2005@gmail.com>
2025-08-26 11:33:08 +0530

    add files

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const {createUser,loginUser ,getAllUser,getUserById ,updateUserById,deleteUserById} = require("../api/user/userController")
+const { createUser, loginUser, getAllUser, getUserById, updateUserById, deleteUserById } = require("../api/user/userController")
 
-router.post("/createuser",createUser)
-router.post("/loginuser",loginUser)
-router.post("/getAlluser",getAllUser)
-router.post("/getuserByid",getUserById)
-router.post("/updateUserById",updateUserById)
-router.post("/deleteUserById",deleteUserById)
+router.post("/createuser", createUser)
+router.post("/loginuser", loginUser)
+router.post("/getAlluser", getAllUser)
+router.post("/getuserByid", getUserById)
+router.post("/updateUserById", updateUserById)
+router.post("/deleteUserById", deleteUserById)
 
 
 
